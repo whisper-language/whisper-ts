e4e466b6619cd8a145e03f22f3719b30e34ada85
jzx <945627077@qq.com>
2021-06-28 13:14:42 +0800

    更新

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
-
-import { ANTLRInputStream, CharStreams, CommonTokenStream } from 'antlr4ts';
-import {WhisperLanguageLexer} from "./gen/WhisperLanguageLexer";
-import  {WhisperLanguageParser} from "./gen/WhisperLanguageParser";
+import { CharStreams, CommonTokenStream } from "antlr4ts";
+import { WhisperLanguageLexer } from "./gen/WhisperLanguageLexer";
+import { WhisperLanguageParser } from "./gen/WhisperLanguageParser";
 import EvalVititor from "./evalVisitor";
-import Scope from './Scope';
+import Scope from "./Scope";
 
-const fs = require('fs');
-const input = fs.readFileSync('./case/main.whl', 'utf8')
-let inputStream =CharStreams.fromString(input);
+const fs = require("fs");
+var input = fs.readFileSync("./case/main.whl", "utf8");
+input = `a=1+1;`;
+
+console.log(input);
+let inputStream = CharStreams.fromString(input);
 let lexer = new WhisperLanguageLexer(inputStream);
 let tokenStream = new CommonTokenStream(lexer);
 let parser = new WhisperLanguageParser(tokenStream);
+parser.buildParseTree = true;
 let tree = parser.parse();
-let scope:Scope=new Scope();
-let evalVititor=new EvalVititor(scope,new Map(),new Map());
-evalVititor.visit(tree);
+let scope: Scope = new Scope();
+let evalVititor = new EvalVititor(scope, new Map(), new Map());
+let ret = evalVititor.visit(tree);
+console.log("结果");
+console.log(ret);
--- src/returnValue.ts
+++ src/returnValue.ts
@@ -1,5 +1,8 @@
 import TLValue from "./TLValue"
 
 export default class ReturnValue {
+    constructor(){
+        this.value=TLValue.NULL;
+    }
     value:TLValue;
 }
